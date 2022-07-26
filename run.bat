1f87b429f55a3a96682565252261d94dfb4ffad5
Laasya rao <pranava.mihi@gmail.com>
2022-07-26 22:04:48 +0530

    first commit

---
+++ run.bat
@@ -0,0 +1,4 @@
+set classpath=lib/commons-math3-3.0.jar;lib/com.google.guava_1.6.0.jar;lib/hadoop-0.20.1-core.jar;lib/org.apache.commons.httpclient.jar;lib/org-apache-commons-logging.jar;lib/jfreechart-1.0.13.jar;lib/jfreechart-1.0.13-experimental.jar;lib/jfreechart-1.0.13-swt.jar;lib/jcommon-1.0.16.jar;lib/mysql-connector-java-5.1.6-bin.jar;lib/Panel.jar;.;
+javac -d . *.java
+java com.BigData
+pause
